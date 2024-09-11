4a6d66d61edcdf53c7baf6d60ed3bbfc03311aa5
369webforall <369webforall@gmail.com>
2024-09-11 14:51:19 +0545

    social app day 3

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import AddPost from "@/components/AddPost";
 import Feed from "@/components/Feed";
 import Stories from "@/components/Stories";
+import RightMenu from "@/components/rightMenu/RightMenu";
+import LeftMenu from "@/components/leftMenu/LeftMenu";
 export default function Home() {
   return (
     <div className="p-6 font-[family-name:var(--font-geist-sans)] flex gap-6">
-      <div className="hidden xl:block w-[20%]">leftSection</div>
+      <div className="hidden xl:block w-[20%]">
+        <LeftMenu />
+      </div>
       <div className="w-full lg:w-[70%] xl:w-[50%]">
         <div className="flex flex-col gap-6">
           <Stories />
@@ -12,7 +16,9 @@ export default function Home() {
           <Feed />
         </div>
       </div>
-      <div className="hidden lg:block w-[30%]">RightSection</div>
+      <div className="hidden lg:block w-[30%]">
+        <RightMenu />
+      </div>
     </div>
   );
 }

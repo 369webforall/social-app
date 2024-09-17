6aadfd64e95606e0431a8265f019e2e5f92607db
369webforall <369webforall@gmail.com>
2024-09-17 15:07:54 +0545

    webhook

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # docker run --name social-app -e POSTGRES_USER=myuser -e POSTGRES_PASSWORD=mypassword -e POSTGRES_DB=mydatabase -p 5434:5432 -d postgres
 
 # DATABASE_URL="postgresql://myuser:mypassword@localhost:5434/social_db?schema=public"
+
+# https://dashboard.ngrok.com/get-started/setup/windows
---
+++ app/profile/[username]/page.tsx
@@ -0,0 +1,99 @@
+import Feed from "@/components/Feed";
+import RightMenu from "@/components/rightMenu/RightMenu";
+import LeftMenu from "@/components/leftMenu/LeftMenu";
+import Image from "next/image";
+import prisma from "@/prisma/client";
+import { notFound } from "next/navigation";
+
+import { auth } from "@clerk/nextjs/server";
+
+export default async function ProfilePage({
+  params,
+}: {
+  params: { username: string };
+}) {
+  const { userId: currentUserId } = auth();
+
+  const user = await prisma.user.findFirst({
+    where: {
+      username: params.username,
+    },
+    include: {
+      _count: {
+        select: { followers: true, followings: true, posts: true },
+      },
+    },
+  });
+  console.log(user);
+  if (!user) return notFound();
+
+  let isBloacked;
+
+  if (currentUserId) {
+    const res = await prisma.block.findFirst({
+      where: {
+        blockerId: user.id,
+        blockedId: currentUserId,
+      },
+    });
+
+    if (res) {
+      isBloacked = true;
+    }
+  } else {
+    isBloacked = false;
+  }
+  if (!isBloacked) notFound();
+
+  return (
+    <div className="p-6 font-[family-name:var(--font-geist-sans)] flex gap-6">
+      <div className="hidden xl:block w-[20%]">
+        <LeftMenu type="profile" />
+      </div>
+      <div className="w-full lg:w-[70%] xl:w-[50%]">
+        <div className="flex flex-col gap-6">
+          <div className="flex flex-col items-center justify-center">
+            <div className="w-full h-64 relative">
+              <Image
+                src={user.cover || "/noCover.png"}
+                alt="nature"
+                fill
+                className="object-cover rounded-md"
+              />
+              <Image
+                src={user.avatar || "/noAvatar.png"}
+                alt="camera"
+                width={128}
+                height={128}
+                className="w-32 h-32 rounded-full absolute left-0 right-0 mx-auto -bottom-16 ring-4 ring-white"
+              />
+            </div>
+            <h1 className="mt-20 mb-4 text-2xl font-medium">
+              {user.name && user.surname
+                ? user.name + " " + user.surname
+                : user.username}
+            </h1>
+            <div className="flex items-center justify-center gap-12 mb-4">
+              <div className="flex flex-col items-center">
+                <span className="font-medium">{user._count.posts}</span>
+                <span className="text-sm">Posts</span>
+              </div>
+              <div className="flex flex-col items-center">
+                <span className="font-medium">{user._count.followers}</span>
+                <span className="text-sm">Followers</span>
+              </div>
+              <div className="flex flex-col items-center">
+                <span className="font-medium">{user._count.followings}</span>
+                <span className="text-sm">Following</span>
+              </div>
+            </div>
+          </div>
+          <Feed />
+        </div>
+      </div>
+      <div className="hidden lg:block w-[30%]">
+        <RightMenu userId="dev" />
+      </div>
+    </div>
+  );
+}
